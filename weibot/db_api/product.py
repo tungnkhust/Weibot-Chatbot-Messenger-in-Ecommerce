bc2fe8a2664b3b9fc98467b1b98d286601a74590
Dang Minh Nguyen <dangnm.cs@gmail.com>
2021-11-29 11:45:56 +0700

    update

--- weibot/db_api/product.py
+++ weibot/db_api/product.py
@@ -3,18 +3,17 @@ from config import APP_PASSWORD, SHOP_URL, API_VERSION
 import shopify
 
 
-def _build_query(sex, product_type, min_price, max_price) -> str:
+def _build_query(gender, product_type, min_price, max_price, color, size) -> str:
     query = ""
 
-    if sex is not None:
+    if gender is not None:
         if len(query) > 0:
             query += " AND "
-        query += f"tag:{sex}"
+        query += f"tag:{gender}"
     
     if product_type is not None:
         if len(query) > 0:
             query += " AND "
-        print(type(product_type))
         if type(product_type) == list:
             query += " AND ".join([f"tag:{pd}" for pd in product_type])   
         if type(product_type) == str:
@@ -29,53 +28,106 @@ def _build_query(sex, product_type, min_price, max_price) -> str:
         if len(query) > 0:
             query += " AND "
         query += f"price:<={max_price}"
+
+    if color is not None:
+        if len(query) > 0:
+            query += " AND "
+        if type(color) == list:
+            query += " AND ".join([f"{c}" for c in color])   
+        if type(color) == str:
+            query += f"{color}"
+
+    if size is not None:
+        if len(query) > 0:
+            query += " AND "
+        if type(size) == list:
+            query += " AND ".join([f"{c}" for c in size])   
+        if type(size) == str:
+            query += f"{size}"
     
     return query
 
-def get_product_variants():
+def get_product_variants_by_product_id(product_id, color=None, size=None, min_price=None, max_price=None):
     with shopify.Session.temp(SHOP_URL, API_VERSION, APP_PASSWORD): 
         response = shopify.GraphQL().execute(
             """
-            {
-                productVariants(first: 10) {
-                    edges {
-                        node {
-                            id
-                            displayName
-                            inventoryQuantity
-                            price
-                            selectedOptions {
-                                name
-                                value
-                            }
-                            product {
+            query ($id: ID!) {
+                product(id: $id) {
+                    variants(first: 100) {
+                        edges {
+                            node {
                                 id
-                                handle
-                                title
-                                vendor
-                                description
-                                productType
-                                tags
-                                images(first: 1) {
-                                    edges {
-                                        node {
-                                            src
-                                        }
-                                    }
+                                displayName
+                                inventoryQuantity
+                                price
+                                selectedOptions {
+                                    name
+                                    value
                                 }
-                            }
-                            image {
-                                src
-                                altText
+                                image {
+                                    src
+                                    altText
+                                }
                             }
                         }
                     }
                 }
             }
-            """
+            """,
+            {
+                "id": product_id
+            }
         )
 
-        return json.loads(response)["data"]["productVariants"]["edges"]
+        variants = json.loads(response)["data"]["product"]["variants"]["edges"]
+
+        to_removes = []
+
+        if color != None:
+            for (idx, variant) in enumerate(variants):
+                if variant["node"]["selectedOptions"][0]["name"] == "Màu sắc" and variant["node"]["selectedOptions"][0]["value"] == color:
+                    pass
+                else:
+                    to_removes.append(idx)
+            for to_remove in sorted(to_removes, reverse=True):
+                del variants[to_remove]
+
+            to_removes = []
+
+        if size != None:
+            for (idx, variant) in enumerate(variants):
+                if variant["node"]["selectedOptions"][1]["name"] == "Kích cỡ" and variant["node"]["selectedOptions"][1]["value"] == size:
+                    pass
+                else:
+                    to_removes.append(idx)
+            for to_remove in sorted(to_removes, reverse=True):
+                del variants[to_remove]
+
+            to_removes = []
+
+        if min_price != None:
+            for (idx, variant) in enumerate(variants):
+                if int(variant["node"]["price"]) >= min_price:
+                    pass
+                else:
+                    to_removes.append(idx)
+            for to_remove in sorted(to_removes, reverse=True):
+                del variants[to_remove]
+
+            to_removes = []
+
+        if max_price != None:
+            for (idx, variant) in enumerate(variants):
+                if int(variant["node"]["price"]) <= max_price:
+                    pass
+                else:
+                    to_removes.append(idx)
+            for to_remove in sorted(to_removes, reverse=True):
+                del variants[to_remove]
+
+            to_removes = []
+
+        return variants
 
 def get_product_variant_by_id(variant_id):
     with shopify.Session.temp(SHOP_URL, API_VERSION, APP_PASSWORD):
@@ -121,10 +173,10 @@ def get_product_variant_by_id(variant_id):
 
         return json.loads(response)["data"]["productVariant"]
 
-def get_products(sex=None, product_type=None, min_price=None, max_price=None):
+def get_products(gender=None, product_type=None, min_price=None, max_price=None, color=None, size=None):
     with shopify.Session.temp(SHOP_URL, API_VERSION, APP_PASSWORD):
         
-        query = _build_query(sex, product_type, min_price, max_price)
+        query = _build_query(gender, product_type, min_price, max_price, color, size)
         
         response = shopify.GraphQL().execute(
             """
@@ -217,4 +269,6 @@ def get_product_by_id(product_id):
         return json.loads(response)["data"]["product"]
 
 if __name__ == "__main__":
-    print(get_products(product_type=["quần short"], sex="nam"))
+    # print(get_products(product_type=["quần short"], gender="nam"))
+    # print(get_product_variants_by_product_id(product_id="gid://shopify/Product/7459202826454", color="Xanh dương", size="L"))
+    print(get_products(product_type="áo len", color="Đỏ", size="S"))
