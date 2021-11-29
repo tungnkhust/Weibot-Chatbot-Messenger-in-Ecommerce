692a4d99f053b6e3db85e83e129c2a5e5145b6ac
Dang Minh Nguyen <dangnm.cs@gmail.com>
2021-11-29 08:12:33 +0700

    add get productByID and get productVariantByID

--- weibot/shopify/product.py
+++ weibot/shopify/product.py
@@ -48,6 +48,7 @@ def getProductVariants():
                                 value
                             }
                             product {
+                                id
                                 handle
                                 title
                                 vendor
@@ -75,17 +76,61 @@ def getProductVariants():
 
         return json.loads(response)["data"]["productVariants"]["edges"]
 
+def getProductVariantByID(variant_id):
+    with shopify.Session.temp(SHOP_URL, API_VERSION, APP_PASSWORD):
+        response = shopify.GraphQL().execute(
+            """
+            query ($id: ID!) {
+                productVariant(id: $id) {
+                    id
+                    displayName
+                    inventoryQuantity
+                    price
+                    selectedOptions {
+                        name
+                        value
+                    }
+                    product {
+                        id
+                        handle
+                        title
+                        vendor
+                        description
+                        productType
+                        tags
+                        images(first: 1) {
+                            edges {
+                                node {
+                                    src
+                                }
+                            }
+                        }
+                    }
+                    image {
+                        src
+                        altText
+                    }
+                }
+            }
+            """,
+            {
+                "id": variant_id
+            }
+        )
+
+        return json.loads(response)["data"]["productVariant"]
+
 def getProducts(sex=None,product_type=None,min_price=None,max_price=None):
     with shopify.Session.temp(SHOP_URL, API_VERSION, APP_PASSWORD):
         
         query = _build_query(sex,product_type,min_price,max_price)
         
         response = shopify.GraphQL().execute(
-            f"""
-            {{
-                products(first: 5, query:"{query}") {{
-                    edges {{
-                        node {{
+            """
+            query($query: String) {
+                products(first: 5, query: $query) {
+                    edges {
+                        node {
                             id
                             handle
                             title
@@ -93,35 +138,104 @@ def getProducts(sex=None,product_type=None,min_price=None,max_price=None):
                             totalInventory
                             productType
                             description
-                            priceRangeV2 {{
-                                minVariantPrice {{
+                            priceRangeV2 {
+                                minVariantPrice {
                                     amount
-                                }}
-                                maxVariantPrice {{
+                                }
+                                maxVariantPrice {
                                     amount
-                                }}
-                            }}
-                            options(first: 3) {{
+                                }
+                            }
+                            options(first: 3) {
                                 name
                                 values
-                            }}
+                            }
                             vendor
-                            images(first: 1) {{
-                                edges {{
-                                    node {{
+                            images(first: 1) {
+                                edges {
+                                    node {
                                         src
                                         altText
-                                    }}
-                                }}
-                            }}
-                        }}
-                    }}
-                }}
-            }}
-            """
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            """,
+            {
+                "query": query
+            }
         )
         
         return json.loads(response)["data"]["products"]["edges"]
 
+def getProductByID(product_id):
+    with shopify.Session.temp(SHOP_URL, API_VERSION, APP_PASSWORD):
+        response = shopify.GraphQL().execute(
+            """
+            query ($id: ID!) {
+                product(id: $id) {
+                    id
+                    handle
+                    title
+                    tags
+                    totalInventory
+                    productType
+                    description
+                    priceRangeV2 {
+                        minVariantPrice {
+                            amount
+                        }
+                        maxVariantPrice {
+                            amount
+                        }
+                    }
+                    options(first: 3) {
+                        name
+                        values
+                    }
+                    vendor
+                    images(first: 1) {
+                        edges {
+                            node {
+                                src
+                                altText
+                            }
+                        }
+                    }
+                }
+            }
+            """,
+            {
+                "id": product_id
+            }
+        )
+
+        return json.loads(response)["data"]["product"]
+
+def getCollections():
+    with shopify.Session.temp(SHOP_URL, API_VERSION, APP_PASSWORD):
+        response = shopify.GraphQL().execute(
+            """
+            {
+                collections(first: 30, query:"-title:Nam AND -title:Nữ AND -title:'Còn hàng'") {
+                    edges {
+                        node {
+                            id
+                            title
+                        }
+                    }
+                }
+            }
+            """
+        )
+
+        return json.loads(response)["data"]["collections"]["edges"]
+
 if __name__ == "__main__":
     print(getProducts(product_type=["quần short"], sex="nam"))
+    # print(getCollections())
+    # print(getProductVariantByID("gid://shopify/ProductVariant/42111435538646"))
+    # print(getProductByID("gid://shopify/Product/7464050983126"))
